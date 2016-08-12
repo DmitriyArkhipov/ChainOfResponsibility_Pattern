401ff7f9401fb6d00ece10f0773d560579a8f73f
Dmitriy Arkhipov <COODi93@mac-admin.local>
2016-08-12 17:56:27 +0800

    Initial Commit

---
+++ chain_of_responsibility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chain_of_responsibility
+//
+//  Created by Dmitriy Arkhipov on 12.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chain_of_responsibility/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chain_of_responsibility
+//
+//  Created by Dmitriy Arkhipov on 12.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
